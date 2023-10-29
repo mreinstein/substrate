169017358dde5593812a4b6f86b67057aae983c6
Mike <reinstein.mike@gmail.com>
2023-10-28 22:18:31 -0700

    update snabby and other deps

--- deps.js
+++ deps.js
@@ -1,5 +1,5 @@
 import drawer     from 'https://cdn.jsdelivr.net/gh/mreinstein/snabbdom-drawer/drawer.js'
-import html       from 'https://cdn.skypack.dev/pin/snabby@v5.0.0-IfdiC3ZbHdp4mlzzhmHK/mode=imports,min/optimized/snabby.js'
+import html       from 'https://cdn.skypack.dev/pin/snabby@v6.0.0-WiE7uSuyNsVpelxkVjF8/mode=imports,min/optimized/snabby.js'
 import escodegen  from 'https://cdn.skypack.dev/pin/escodegen@v2.0.0-JMiqpk423LP85CCX8Asm/mode=imports/optimized/escodegen.js'
 import * as acorn from 'https://cdn.skypack.dev/pin/acorn@v8.7.0-niLhPC1wKoeipqaFA3RM/mode=imports,min/optimized/acorn.js'
 import { marked } from 'https://cdn.skypack.dev/pin/marked@v4.0.10-6MvNv8uZ3x5w2Hq7jYAA/mode=imports,min/optimized/marked.js'
