b1e6eff1255ecfe33951d40245e08142bb739811
Mike <reinstein.mike@gmail.com>
2023-11-08 11:34:44 -0800

    update deps

--- deps.js
+++ deps.js
@@ -1,7 +1,7 @@
 import drawer     from 'https://cdn.jsdelivr.net/gh/mreinstein/snabbdom-drawer/drawer.js'
-import html       from 'https://cdn.skypack.dev/pin/snabby@v6.0.0-WiE7uSuyNsVpelxkVjF8/mode=imports,min/optimized/snabby.js'
-import escodegen  from 'https://cdn.skypack.dev/pin/escodegen@v2.0.0-JMiqpk423LP85CCX8Asm/mode=imports/optimized/escodegen.js'
-import * as acorn from 'https://cdn.skypack.dev/pin/acorn@v8.7.0-niLhPC1wKoeipqaFA3RM/mode=imports,min/optimized/acorn.js'
+import html       from 'https://cdn.skypack.dev/pin/snabby@v6.1.0-zapIMSOZjVJrawoQIkB4/mode=imports,min/optimized/snabby.js'
+import escodegen  from 'https://cdn.skypack.dev/pin/escodegen@v2.1.0-GJF0B2OrzRTMBIndFwkQ/mode=imports,min/optimized/escodegen.js'
+import * as acorn from 'https://cdn.skypack.dev/pin/acorn@v8.11.2-gFWpLMxrbCQIys4dAWIf/mode=imports,min/optimized/acorn.js'
 import { marked } from 'https://cdn.skypack.dev/pin/marked@v4.0.10-6MvNv8uZ3x5w2Hq7jYAA/mode=imports,min/optimized/marked.js'
 import { escape } from 'https://cdn.skypack.dev/pin/html-escaper@v3.0.3-VC8DkvrPfDlbCUfqgg40/mode=imports/optimized/html-escaper.js'
 
